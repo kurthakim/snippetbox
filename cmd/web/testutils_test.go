eac3cd65202f876bb5f1a516319af65e408bda7e
Kurt Hakim <kurthakim@gmail.com>
2022-11-22 19:47:15 +0700

    finished ch14 - mocking- initializing the mocks

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -5,14 +5,36 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
 	"net/http/httptest"
 	"testing"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/go-playground/form/v4"
+	"snippetbox.kurt.net/internal/models/mocks"
 )
 
 func newTestApplication(t *testing.T) *application {
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	formDecoder := form.NewDecoder()
+
+	sessionManager := scs.New()
+	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
+
 	return &application {
 		errorLog: log.New(io.Discard, "", 0),
 		infoLog: log.New(io.Discard, "", 0),
+		snippets: &mocks.SnippetModel{},
+		users: &mocks.UserModel{},
+		templateCache: templateCache,
+		formDecoder: formDecoder,
+		sessionManager: sessionManager,
 	}
 }
 
@@ -22,6 +44,18 @@ type testServer struct {
 
 func newTestServer(t *testing.T, h http.Handler) *testServer {
 	ts := httptest.NewTLSServer(h)
+
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ts.Client().Jar = jar
+
+	ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
 	return &testServer{ts}
 }
 
